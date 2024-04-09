815973a136eaacfb1a2338e388b7f0d37b5a0a2f
Thomas <60945086+thomas-chernaik@users.noreply.github.com>
2024-04-09 01:53:06 +0100

    added a bunch more ones

--- AddQuestion.py
+++ AddQuestion.py
@@ -1,9 +1,18 @@
 phrase = input("Please enter the phrase you would like to add: ")
-context = input("Please enter the clue for the phrase: ")
 import json
 with open('jumble.json', 'r') as f:
     data = json.load(f)
-    data.append({"phrase": phrase, "context": context})
+# create a set of lower case phrases
+phrases = set()
+for item in data:
+    phrases.add(item["phrase"].lower())
+if phrase.lower() in phrases:
+    print("Phrase already exists!")
+    exit()
+context = input("Please enter the clue for the phrase: ")
+data.append({"phrase": phrase, "context": context})
 with open('jumble.json', 'w') as f:
     json.dump(data, f)
 print("Phrase added successfully!")
+# print the number of phrases in the file
+print("Number of phrases in the file: ", len(data))
